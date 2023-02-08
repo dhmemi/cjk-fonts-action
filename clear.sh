281090f0e675bc9ffa3d8f222d7a66be766e0bb1
RisingInIris2017 <risinginiris2017@gmail.com>
2023-02-09 00:54:17 +0800

    fix clear script

--- clear.sh
+++ clear.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -e "SourceHanSansHWSC.zip"] ; then
+if [ -e "SourceHanSansHWSC.zip" ] ; then
     rm SourceHanSansHWSC.zip  
 fi
-if [ -d "Open_Data"] ; then
+if [ -d "Open_Data" ] ; then
     rm -r Open_Data
 fi
