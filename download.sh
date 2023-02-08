85d509c6c086f0c1356bf92884f8046ef8d71c6b
RisingInIris2017 <risinginiris2017@gmail.com>
2023-02-08 23:46:11 +0800

    改回总是下载字体

--- download.sh
+++ download.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-hasRegular= [ -e "$HOME/.fonts/SourceHanSansHWSC-Regular.otf" ]
-hasBold= [ -e "$HOME/.fonts/SourceHanSansHWSC-Bold.otf"]
+# Download
+while getopts 'f:' flag; do
+  case "${flag}" in
+    f) flags="${OPTARG}" ;;
+    *) print_usage
+      exit 1 ;;
+  esac
+done
 
-if [ !$hasRegular -o !$hasBold ] ; then
+wget -O SourceHanSansHWSC.zip ${flags} https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansHWSC.zip
+unzip SourceHanSansHWSC.zip
 
-  # Download
-  while getopts 'f:' flag; do
-    case "${flag}" in
-      f) flags="${OPTARG}" ;;
-      *) print_usage
-        exit 1 ;;
-    esac
-  done
-
-  wget -O SourceHanSansHWSC.zip ${flags} https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansHWSC.zip
-  unzip SourceHanSansHWSC.zip
-
-  # Copy fonts to font directory
-  cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Regular.otf $HOME/.fonts
-  cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Bold.otf $HOME/.fonts
-fi
+# Copy fonts to font directory
+cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Regular.otf $HOME/.fonts
+cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Bold.otf $HOME/.fonts
